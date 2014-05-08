fd801c87a7f9d157a42c892ee5db044b96ae3d50
skyzhaoting <skyzhao89@qq.com>
2014-05-08 15:54:24 +0800

    IsRotation

---
+++ arraysandstring/IsRotation.java
@@ -0,0 +1,18 @@
+package arraysandstrings;
+
+public class IsRotation
+{
+	/**
+	 * 给点两个字符串 s1 s2 判断s2是不是为s1旋转而来
+	 * 将字符串s1 -- > s1s1 判断s2是不是s1的子串
+	 */
+	public static boolean isRotation(String s1,String s2)
+	{
+		if(s1.length() == s2.length() && s1.length() > 0)
+		{
+			String ss = s1 + s1;
+			return isSubstring(ss, s2);
+		}
+		return false;
+	}
+}
